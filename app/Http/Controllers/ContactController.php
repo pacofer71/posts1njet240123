674b44b68a7a2b7265d46a6305a8edc4404e3d15
Francisco Fernández-Arévalo Collado <pacofer71@gmail.com>
2023-01-26 08:41:22 +0100

    Capturando excepción en mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,24 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ContactMailable;
 use Illuminate\Http\Request;
+use App\Mail\ContactMailable;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('contact.index');
     }
 
-    public function send(Request $request){
+    public function send(Request $request)
+    {
         $request->validate([
-            'nombre'=>['required', 'string', 'min:3'],
-            'email'=>['required', 'email'],
-            'mensaje'=>['required', 'string', 'min:5', 'max:250'],
+            'nombre' => ['required', 'string', 'min:3'],
+            'email' => ['required', 'email'],
+            'mensaje' => ['required', 'string', 'min:5', 'max:250'],
         ]);
-        
-        Mail::to('adminsitio@gmail.com')->send(new ContactMailable($request->all()));
+
+        try {
+            Mail::to('adminsitio@gmail.com')->send(new ContactMailable($request->all()));
+        } catch (\Exception $exception) {
+            return redirect()->route('dashboard')->with('info', 'No se pudo enviar el mensaje, inténtelo más tarde');
+            
+        }
         return redirect()->route('dashboard')->with('info', 'mensaje enviado');
     }
+    
 }
